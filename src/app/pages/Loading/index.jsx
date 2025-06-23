5a077cd222d74895d9318d552bbbe2e13e21de8e
Phan Xuan Hoai Nam <pxhnam@gmail.com>
2025-06-23 10:26:53 +0700

    fix: update gradient colors in LoadingPage for improved visual appeal

--- src/app/pages/Loading/index.jsx
+++ src/app/pages/Loading/index.jsx
@@ -3,7 +3,7 @@ import { APP_NAME } from '~/config/env';
 const LoadingPage = () => {
     return (
         <div className='w-full h-screen flex justify-center items-center bg-white -mt-[130px]'>
-            <span className='text-3xl font-black uppercase bg-gradient-to-r from-[#4799e9] via-[#65f4cd] to-[#ff4ecd] bg-[length:300%_100%] bg-clip-text text-transparent animate-gradient'>
+            <span className='text-3xl font-black uppercase bg-gradient-to-r from-[#4799e9] to-[#65f4cd] bg-[length:300%_100%] bg-clip-text text-transparent animate-gradient'>
                 {APP_NAME}
             </span>
         </div>
