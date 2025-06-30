cc1d52a617742dbf6c73eed99aeaeef6df75f322
Phan Xuan Hoai Nam <pxhnam@gmail.com>
2025-06-30 20:14:13 +0700

    fix: improve layout and responsiveness in LecturerCard component

--- src/app/sections/LecturerCourse/LecturerCard.jsx
+++ src/app/sections/LecturerCourse/LecturerCard.jsx
@@ -5,14 +5,14 @@ import { Facebook, In } from '~/components/Icons';
 const LecturerCard = ({ avatar, name, expertise, bio, facebook, linkedIn }) => {
     return (
         <div className='flex flex-col xl:flex-row'>
-            <div className='flex-1 flex'>
+            <div className='w-full xl:w-2/5 flex'>
                 <Image
                     src={avatar}
                     alt={name}
-                    className='w-full rounded-t-3xl xl:rounded-tr-none xl:rounded-s-3xl object-cover'
+                    className='w-full aspect-[4/3] xl:aspect-[3/4] rounded-t-3xl xl:rounded-tr-none xl:rounded-s-3xl object-cover'
                 />
             </div>
-            <div className='flex-[1.5] border border-gray-400 border-t-0 xl:border-t xl:border-s-0 rounded-b-3xl xl:rounded-bl-none xl:rounded-e-3xl p-5'>
+            <div className='flex-1 flex flex-col justify-between border border-gray-400 border-t-0 xl:border-t xl:border-s-0 rounded-b-3xl xl:rounded-bl-none xl:rounded-e-3xl gap-5 p-5'>
                 <div className='flex flex-col items-center xl:items-start'>
                     <h3 className='heading-4 mb-2'>{name}</h3>
                     <span className='text-paragraph font-bold'>
@@ -21,7 +21,7 @@ const LecturerCard = ({ avatar, name, expertise, bio, facebook, linkedIn }) => {
                     <div className='w-full border-t border-gray-400 border-dashed my-1.5' />
                     <p className='text-paragraph'>{bio}</p>
                 </div>
-                <div className='flex justify-center xl:justify-end gap-3 mt-5'>
+                <div className='flex justify-center xl:justify-end gap-3'>
                     <Button
                         href={facebook}
                         primary
