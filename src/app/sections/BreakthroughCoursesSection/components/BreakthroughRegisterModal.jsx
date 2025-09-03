669c0050574cdb2c7f7735576c2567717100295c
Phan Xuan Hoai Nam <pxhnam@gmail.com>
2025-09-03 19:28:06 +0700

    feat: update BreakthroughRegisterModal layout and styling for improved user experience

--- src/app/sections/BreakthroughCoursesSection/components/BreakthroughRegisterModal.jsx
+++ src/app/sections/BreakthroughCoursesSection/components/BreakthroughRegisterModal.jsx
@@ -1,13 +1,11 @@
 import PropTypes from 'prop-types';
 import { Button, Image, Modal } from '~/components/UI';
-import { cn } from '~/utils';
 import DA1 from '~/assets/images/DA2025-1.png';
 import DA2 from '~/assets/images/DA2025-2.png';
 import DA3 from '~/assets/images/DA2025-3.png';
 import DA4 from '~/assets/images/DA2025-4.png';
 import DA5 from '~/assets/images/DA2025-5.png';
 import { Fire } from '~/components/Icons';
-import { Link } from 'react-router-dom';
 
 const images = [DA1, DA2, DA3, DA4, DA5];
 
@@ -19,7 +17,7 @@ function BreakthroughRegisterModal({ isOpen, onClose }) {
             center
             className='relative w-[36rem] max-h-[90vh] max-w-[90vw] flex flex-col bg-white rounded-2xl'
         >
-            <div className='h-10 flex justify-end'>
+            {/* <div className='h-10 flex justify-end'>
                 <div className='size-10 flex justify-center items-center'>
                     <button
                         className={cn(
@@ -30,38 +28,30 @@ function BreakthroughRegisterModal({ isOpen, onClose }) {
                         onClick={onClose}
                     />
                 </div>
-            </div>
-            <div className='flex-1 flex flex-col xl:px-10 px-5 overflow-hidden'>
-                <h3 className='heading-3 leading-normal uppercase'>
-                    Đăng ký khóa học{' '}
-                    <span className='text-primary whitespace-nowrap'>
-                        Data analysis
-                    </span>
-                </h3>
-                <p className='text-sm font-bold flex items-end gap-1'>
-                    <span>Đừng bỏ lỡ cơ hội bứt phá sự nghiệp</span>
-                    <Fire />
-                </p>
-                <p className='text-sm mt-1'>
-                    Bạn có biết,{' '}
-                    <span className='font-bold'>
-                        hơn 20 khóa học Phân tích Dữ liệu
-                    </span>{' '}
-                    của chúng tôi đã khai giảng và giúp hàng nghìn người tìm
-                    được việc làm mơ ước? Hãy khám phá ngay để nâng tầm kỹ năng,
-                    tăng tốc thu nhập.
-                    <br />
-                    <span className='text-base font-bold'>
-                        Đăng ký tại:
-                    </span>{' '}
-                    <Link
-                        to='/'
-                        className='text-blue-400 hover:underline underline-offset-2'
-                    >
-                        www.trendemy.com.vn
-                    </Link>
-                </p>
-                <div className='flex overflow-hidden my-3'>
+            </div> */}
+            <div className='flex-1 flex flex-col overflow-hidden xl:px-10 p-5 gap-5'>
+                <div>
+                    <h3 className='heading-3 leading-normal uppercase mb-2'>
+                        Đăng ký khóa học{' '}
+                        <span className='text-primary whitespace-nowrap'>
+                            Data analysis
+                        </span>
+                    </h3>
+                    <p className='text-sm font-bold flex items-end gap-1'>
+                        <span>Đừng bỏ lỡ cơ hội bứt phá sự nghiệp</span>
+                        <Fire />
+                    </p>
+                    <p className='text-sm mt-px'>
+                        Bạn có biết,{' '}
+                        <span className='font-bold'>
+                            hơn 20 khóa học Phân tích Dữ liệu
+                        </span>{' '}
+                        của chúng tôi đã khai giảng và giúp hàng nghìn người tìm
+                        được việc làm mơ ước? Hãy khám phá ngay để nâng tầm kỹ
+                        năng, tăng tốc thu nhập.
+                    </p>
+                </div>
+                <div className='flex overflow-hidden'>
                     <div className='flex-1 flex aspect-square flex-col overflow-y-auto'>
                         {images.map((image) => (
                             <Image
@@ -73,7 +63,7 @@ function BreakthroughRegisterModal({ isOpen, onClose }) {
                         ))}
                     </div>
                 </div>
-                <div className='flex justify-center items-center gap-8 mb-5'>
+                <div className='flex justify-center items-center gap-8'>
                     <Button
                         primary
                         onClick={() => {
