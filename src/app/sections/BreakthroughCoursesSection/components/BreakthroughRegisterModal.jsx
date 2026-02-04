f2bc943e7e0ea37137afdcee8b3a727c21b856bc
Phan Xuan Hoai Nam <pxhnam@gmail.com>
2026-02-04 12:53:15 +0700

    fix(breakthrough-modal): make title prop optional in PropTypes

--- src/app/sections/BreakthroughCoursesSection/components/BreakthroughRegisterModal.jsx
+++ src/app/sections/BreakthroughCoursesSection/components/BreakthroughRegisterModal.jsx
@@ -3,15 +3,16 @@ import { Button, Image, Modal } from '~/components/UI';
 import { Fire } from '~/components/Icons';
 
 function BreakthroughRegisterModal({ data, isOpen, onClose }) {
-   const { title = '', sub = '', desc = '', images = [], link = '' } = data;
-   return (
-      <Modal
-         open={isOpen}
-         onClose={onClose}
-         center
-         className='relative w-[36rem] max-h-[90vh] max-w-[90vw] flex flex-col bg-white rounded-2xl'
-      >
-         {/* <div className='h-10 flex justify-end'>
+  const { title = '', sub = '', desc = '', images = [], link = '' } = data;
+
+  return (
+    <Modal
+      open={isOpen}
+      onClose={onClose}
+      center
+      className='relative w-[36rem] max-h-[90vh] max-w-[90vw] flex flex-col bg-white rounded-2xl'
+    >
+      {/* <div className='h-10 flex justify-end'>
                 <div className='size-10 flex justify-center items-center'>
                     <button
                         className={cn(
@@ -23,69 +24,67 @@ function BreakthroughRegisterModal({ data, isOpen, onClose }) {
                     />
                 </div>
             </div> */}
-         <div className='flex-1 flex flex-col overflow-hidden xl:px-10 p-5 gap-5'>
-            <div>
-               <h3 className='heading-3 leading-normal uppercase mb-2'>
-                  Đăng ký khóa học{' '}
-                  <span className='text-primary whitespace-nowrap'>
-                     {title}
-                  </span>
-               </h3>
-               <p className='text-sm font-bold flex items-end gap-1'>
-                  <span>{sub}</span>
-                  <Fire />
-               </p>
-               <p className='text-sm mt-px'>
-                  {/* Bạn có biết,{' '}
+      <div className='flex-1 flex flex-col overflow-hidden xl:px-10 p-5 gap-5'>
+        <div>
+          <h3 className='heading-3 leading-normal uppercase mb-2'>
+            Đăng ký khóa học{' '}
+            <span className='text-primary whitespace-nowrap'>{title}</span>
+          </h3>
+          <p className='text-sm font-bold flex items-end gap-1'>
+            <span>{sub}</span>
+            <Fire />
+          </p>
+          <p className='text-sm mt-px'>
+            {/* Bạn có biết,{' '}
                   <span className='font-bold'>
                      hơn 20 khóa học Phân tích Dữ liệu
                   </span>{' '}
                   của chúng tôi đã khai giảng và giúp hàng nghìn người tìm được
                   việc làm mơ ước? Hãy khám phá ngay để nâng tầm kỹ năng, tăng
                   tốc thu nhập. */}
-                  {desc}
-               </p>
-            </div>
-            <div className='flex overflow-hidden'>
-               <div className='flex-1 flex aspect-square flex-col overflow-y-auto'>
-                  {images?.map((image) => (
-                     <Image
-                        key={image}
-                        src={image}
-                        alt={image}
-                        className='w-full object-contain'
-                     />
-                  ))}
-               </div>
-            </div>
-            <div className='flex justify-center items-center gap-8'>
-               <Button
-                  primary
-                  onClick={() => {
-                     window.open(link, '_blank');
-                  }}
-               >
-                  Đăng ký ngay
-               </Button>
-               <Button outline primary onClick={onClose}>
-                  Hủy
-               </Button>
-            </div>
-         </div>
-      </Modal>
-   );
+            {desc}
+          </p>
+        </div>
+        <div className='flex overflow-hidden'>
+          <div className='flex-1 flex aspect-square flex-col overflow-y-auto'>
+            {images?.map((image) => (
+              <Image
+                key={image}
+                src={image}
+                alt={image}
+                className='w-full object-contain'
+              />
+            ))}
+          </div>
+        </div>
+        <div className='flex justify-center items-center gap-8'>
+          <Button
+            primary
+            onClick={() => {
+              window.open(link, '_blank');
+            }}
+          >
+            Đăng ký ngay
+          </Button>
+          <Button outline primary onClick={onClose}>
+            Hủy
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  );
 }
 
 BreakthroughRegisterModal.propTypes = {
-   data: PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      sub: PropTypes.string,
-      desc: PropTypes.string,
-      images: PropTypes.arrayOf(PropTypes.string),
-      link: PropTypes.string
-   }),
-   isOpen: PropTypes.bool,
-   onClose: PropTypes.func
+  data: PropTypes.shape({
+    title: PropTypes.string,
+    sub: PropTypes.string,
+    desc: PropTypes.string,
+    images: PropTypes.arrayOf(PropTypes.string),
+    link: PropTypes.string
+  }),
+  isOpen: PropTypes.bool,
+  onClose: PropTypes.func
 };
 
 export default BreakthroughRegisterModal;
